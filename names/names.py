cce7009ddfd27f3d24b52cd6da1640423ce99012
Arron M <marshall.arron@gmail.com>
2019-09-20 08:56:17 -0700

    Optimize names.py, additional duplicates

--- names/names.py
+++ names/names.py
@@ -10,13 +10,38 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# 64 duplicates
 duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+# Truly 124 duplicates total
+
+# create dictionary
+unique = {}
+
+# loop over recording unique names
+for i in range(max(len(names_1), len(names_2))):
+    if names_1[i] == names_2[i]:
+        unique[names_1[i]] = True
+        duplicates.append(names_1[i])
+
+    # check for duplicate, add to list
+    if names_1[i] in unique.keys():
+        duplicates.append(names_1[i])
+
+    if names_2[i] in unique.keys():
+        duplicates.append(names_2[i])
+    
+    unique[names_1[i]] = True
+    unique[names_2[i]] = True
+
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# if __name__ == '__main__':
+    # print(len(names_1), len(names_2))
