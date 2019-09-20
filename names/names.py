219c40b515fa3a9e74732f44932c232265bfc5f0
Arron M <marshall.arron@gmail.com>
2019-09-20 09:43:36 -0700

    Update comments for clarity

--- names/names.py
+++ names/names.py
@@ -12,10 +12,13 @@ f.close()
 
 # 64 duplicates
 duplicates = []
+
+# ORIGINAL:
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
 
 # Truly 124 duplicates total
 # create dictionary
@@ -38,31 +41,35 @@ duplicates = []
 #     unique[names_1[i]] = True
 #     unique[names_2[i]] = True
 
-# Sprint Solution
+
+# SPRINT SOLUTION:
 # unique = { i : True for i in names_1 }
 
 # for i in range(len(names_2)):
 #     if names_2[i] in unique.keys():
 #         duplicates.append(names_2[i])
 
-# Stretch Solution
-# In place sort to save memory
+
+# STRETCH SOLUTION:
 from bisect import bisect_left
 
+# binary search with bisect
 def binary_search(list, item, low=0, high=None):
     high = high if high is not None else len(list)
     pos = bisect_left(list, item, low, high)
     return (pos if pos != high and list[pos] == item else -1)
 
+# In place sort to save memory
 names_2.sort()
 
+# Search for every name in list 1, to see if it's in list 2
 for name in names_1:
     name_2 = names_2[binary_search(names_2, name)]
     if name_2 == name:
         duplicates.append(name_2)
 
 
-# Alternative solution
+# ALTERNATE SOLUTION:
 # set_1 = set(names_1)
 # set_2 = set(names_2)
 # duplicates = [i for i in set_1.intersection(set_2)]
